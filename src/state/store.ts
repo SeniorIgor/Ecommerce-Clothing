1950923c3df6a5da9a42bffe84b775c86948e3e4
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2021-09-06 21:26:43 +0300

    Removimg redux-logger for production

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,11 +1,16 @@
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import thunk, { ThunkMiddleware } from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 
-import { reducer } from './reducer';
+import { reducer, RootState } from './reducer';
+import { Action } from './index';
 
-const middlewares = [logger, thunk];
+const middlewares = [thunk as ThunkMiddleware<RootState, Action>];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(reducer, {}, applyMiddleware(...middlewares));
 
