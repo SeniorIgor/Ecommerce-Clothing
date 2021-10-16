f4b0665dee94f02b3f06f231a9c9693323e32b86
SeniorIgor <igor.tyapkin1996@yandex.ru>
2021-10-16 20:42:46 +0300

    Delete dist folder

--- dist/routes/payment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const payment_1 = require("../controllers/payment");
-const router = (0, express_1.Router)();
-router.post('/', payment_1.makePayment);
-exports.default = router;
